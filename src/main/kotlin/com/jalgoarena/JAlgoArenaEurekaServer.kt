5cc40400a97b45b39649bb3af222b614c71f71b3
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-29 18:37:16 +0100

    - new feature - enable hystrix dashboard

--- src/main/kotlin/com/jalgoarena/JAlgoArenaEurekaServer.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaEurekaServer.kt
@@ -3,9 +3,11 @@ package com.jalgoarena
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableHystrixDashboard
 open class JAlgoArenaEurekaServer
 
 fun main(args: Array<String>) {
